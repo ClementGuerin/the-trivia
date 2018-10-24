4db663bbcd6146fd08a2fab04ad3ee23400a1ef6
david <sephi@DESKTOP-OCBDTI6.localdomain>
2018-10-24 18:12:54 +0200

    add router to starter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from '../../views/Home/Home';
+import CategoryContainer from '../../views/Category/CategoryContainer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        coucou        
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/categories/:category" component={CategoryContainer} />
+          </div>
+        </Router>
       </div>
     );
   }
